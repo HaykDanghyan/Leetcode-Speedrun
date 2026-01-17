92b196475b03dde1edf7fbc4dd52e37002ff3186
HaykDanghyan <hayk.danghyaan@gmail.com>
2026-01-16 22:03:44 -0500

    Problem 876 - Middle of the Linked List - Easy

---
+++ Linked Lists/876. Middle of the Linked List/README.md
@@ -0,0 +1,19 @@
+## Problem
+[Problem Name](https://leetcode.com/problems/middle-of-the-linked-list)
+
+## Description
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+## Example
+Input: head = [1, 2, 3, 4, 5]
+Output: [3, 4, 5]
+
+Input: head = [1, 2, 3, 4, 5, 6]
+Output:  [4, 5, 6]
+
+
+## Complexity
+Time: O(n)
+Space: O(1)
---
+++ Linked Lists/876. Middle of the Linked List/solution.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+       
+        # two pointer approach - fast and slow
+        # t/c O(n), s/c O(1)
+
+        slow = head
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        return slow
